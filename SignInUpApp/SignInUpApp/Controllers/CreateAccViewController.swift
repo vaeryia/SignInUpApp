f1b5a279e9bac2708694319ab076bddd7dd7831e
vaeryia <moyseychik.v@gmail.com>
2023-08-29 08:24:04 +0300

    Finished SignUp screen

--- SignInUpApp/SignInUpApp/Controllers/CreateAccViewController.swift
+++ SignInUpApp/SignInUpApp/Controllers/CreateAccViewController.swift
@@ -26,9 +26,9 @@ class CreateAccViewController: UIViewController {
 //scrollView
     @IBOutlet weak var scrollView: UIScrollView!
     
-    private var isValidEmail = false
-    private var isConfPassword = false
- //   private var passwordStrength
+    private var isValidEmail = false { didSet { updateContBtnState() }}
+    private var isConfPassword = false { didSet { updateContBtnState() }}
+    private var passwordStrength: PasswordStrength = .veryWeak { didSet { updateContBtnState() }}
 
     
     
@@ -45,24 +45,69 @@ class CreateAccViewController: UIViewController {
     @IBAction func emailTFAction(_ sender: UITextField) {
         if let email = sender.text,
            !email.isEmpty,
-           VerificationService.isValidEmail(email: email) {
+           VerificationService.isValidEmail(email: email)
+        {
             isValidEmail = true
         } else {
             isValidEmail = false
-             }
+        }
         errorEmailLbl.isHidden = isValidEmail
     }
     
     @IBAction func passTFAction(_ sender: UITextField) {
+        if let passText = sender.text,
+           !passText.isEmpty
+        {
+            passwordStrength = VerificationService.isValidPassword(pass: passText)
+        } else {
+            passwordStrength = .veryWeak
+        }
+        errorPassLbl.isHidden = passwordStrength != .veryWeak
+        setupStrognIndicatorsViews()
     }
     
     @IBAction func confPassTFAtion(_ sender: UITextField) {
+        if let confPassText = sender.text,
+           !confPassText.isEmpty,
+           let passText = passwordTF.text,
+           !passText.isEmpty
+        {
+            isConfPassword = VerificationService.isPassConfirm(pass1: passText, pass2: confPassText)
+        } else {
+            isConfPassword = false
+        }
+        errorConPassLbl.isHidden = isConfPassword
     }
     
+    @IBAction func signInAction() {
+        navigationController?.popToRootViewController(animated: true)
+    }
     
+    @IBAction func contAction() {
+        if let email = emailTF.text,
+           let pass = passwordTF.text {
+            let userModel = UserModel(name: nameTF.text, email: email, pass: pass)
+            //TODO: go to the next screen
+        }
+    }
     
+    private func setupStrognIndicatorsViews() {
+        for (index, view) in strongPasswordIndicatoesViews.enumerated() {
+            if index <= (passwordStrength.rawValue - 1) {
+                view.alpha = 1
+            } else {
+                view.alpha = 0.2
+            }
+        }
+//
+//        strongPasswordIndicatoesViews.enumerated().forEach {index, view in
+//
+//        }
+    }
     
-    
+    private func updateContBtnState() {
+        continueBtn.isEnabled = isValidEmail && isConfPassword && passwordStrength != .veryWeak
+    }
     
     private func startKeyboardObserver() {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
@@ -84,28 +129,11 @@ class CreateAccViewController: UIViewController {
         scrollView.scrollIndicatorInsets = contentInsets
     }
     
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
